9cb348e2bfd144a82f1cef99b24cc9c8006aae19
ngoctram168 <minhthanghcm2003@gmail.com>
2025-04-13 05:41:11 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import "./styles.css";
-import { useState } from "react";
+import './styles.css';
+import { useState } from 'react';
 export default function App() {
   return (
     <div>
-      <TextExpander
-        expanded={true}
-        collapsedNumWords={20}
-        expandButtonText="Show text"
-        collapseButtonText="Show less"
-        buttonColor="blue"
-      >
+      <TextExpander>
         1.Space travel is the ultimate adventure! Imagine soaring past the stars
         and exploring new worlds. It's the stuff of dreams and science fiction,
         but believe it or not, space travel is a real thing. Humans and robots
@@ -17,12 +11,7 @@ export default function App() {
         push the boundaries of what's possible.
       </TextExpander>
 
-      <TextExpander
-        collapsedNumWords={20}
-        expandButtonText="Show text"
-        collapseButtonText="Collapse text"
-        buttonColor="#ff6622"
-      >
+      <TextExpander collapseButtonText='Collapse text' buttonColor='#ff6622'>
         2.Space travel requires some seriously amazing technology and
         collaboration between countries, private companies, and international
         space organizations. And while it's not always easy (or cheap), the
@@ -30,17 +19,11 @@ export default function App() {
         foot on the moon or when rovers were sent to roam around on Mars.
       </TextExpander>
 
-      <TextExpander
-        expanded={true}
-        collapsedNumWords={20}
-        expandButtonText="Show text"
-        collapseButtonText="Show less"
-        buttonColor="blue"
-      >
+      <TextExpander expanded={true}>
         3.Space missions have given us incredible insights into our universe and
         have inspired future generations to keep reaching for the stars. Space
-        travel is a pretty cool thing to think about. Who knows what we'll
-        discover next!
+        travel is a pretty cool thing to think about. Who knows what
+        we'sdasdassdasdas discover next!
       </TextExpander>
     </div>
   );
@@ -48,22 +31,24 @@ export default function App() {
 
 function TextExpander({
   children,
-  color,
-  text,
   expanded,
-  collapsedNumWords,
-  expandButtonText,
-  collapseButtonText,
-  buttonColor,
+  collapsedNumWords = 20,
+  expandButtonText = 'Show text',
+  collapseButtonText = 'Show less',
+  buttonColor = 'blue',
 }) {
   const [isOpen, setIsOpen] = useState(expanded);
-  console.log(children.length);
+
+  const fullText = children;
+  const shortText =
+    children.split(' ').slice(0, collapsedNumWords).join(' ') + '...'; //Đầu tiên sẽ chuyển string thành mảng. Sau đó cắt từ vị trí 0 - 20. Tiếp theo là nối cái mảng lại thành chuỗi
+
   function handleToggle() {
     setIsOpen((isOpen) => !isOpen);
   }
   return (
-    <div>
-      {isOpen ?? children}
+    <div className='box'>
+      {isOpen ? fullText : shortText}
       <span style={{ color: buttonColor }} onClick={handleToggle}>
         {isOpen ? collapseButtonText : expandButtonText}
       </span>
